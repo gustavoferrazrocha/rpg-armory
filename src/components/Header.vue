4a72e153d23d173c3d0ca2fc33ced21be8731e75
gustavoferrazrocha <gustavoguusfr@gmail.com>
2021-12-18 16:53:48 -0300

    feat: create header component

---
+++ src/components/Header.vue
@@ -0,0 +1,66 @@
+<template>
+  <nav class="site-header">
+    <div class="container">
+      <div class="header-logo">
+        <button  class="header-logo-btn">
+          <img class="logo-header" src="../assets/logo.png" alt="">
+          <span class="header-title">Rpg Armory</span>
+        </button>
+      </div>
+      <div class="nav-list">
+        <ul>
+          <a href="#">
+            <li>Armas</li>
+          </a>
+          <a href="#">
+            <li>Armaduras</li>
+          </a>
+          <a href="#">
+            <li>Bugigangas</li>
+          </a>
+          <a href="#">
+            <li>Seus Itens</li>
+          </a>
+          <a href="#">
+           <li>Crie seus Itens</li>
+          </a>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.site-header{
+  min-width: 100%;
+}
+.header-logo-btn{
+  display: flex;
+  align-items: center;
+}
+.logo-header{
+  max-width: 2rem;
+}
+.header-title{
+  font-weight: 900;
+}
+.container {
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+}
+ul{
+  display: flex;
+  list-style: none;
+}
+li{
+  margin: 0 1rem;
+  color: white;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
+import Header from '@/components/Header.vue'
 @Component({
   components: {
-    HelloWorld,
+    Header,
   },
 })
 export default class Home extends Vue {}
