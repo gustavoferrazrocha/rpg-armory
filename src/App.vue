231ecc8772a4d99e2f73ea7203bb0f05ffb3b732
gustavoferrazrocha <gustavoguusfr@gmail.com>
2021-12-18 16:54:09 -0300

    feat: new logo and favIcon

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <template>
 	<div id="app">
-		<div id="nav">
-			<router-link to="/">Home</router-link> |
-			<router-link to="/about">About</router-link>
-		</div>
-		<router-view />
-	</div>
+  <!-- App.vue -->
+    <v-app>
+      <!-- <v-navigation-drawer app>
+        
+      </v-navigation-drawer> -->
+
+      <v-app-bar dark color="primary" app>
+        <Header />
+      </v-app-bar>
+
+      <!-- Sizes your content based upon application components -->
+      <v-main>
+
+        <!-- Provides the application the proper gutter -->
+        <v-container fluid>
+
+          <!-- If using vue-router -->
+          <router-view></router-view>
+        </v-container>
+      </v-main>
+
+      <v-footer app>
+        <!-- -->
+      </v-footer>
+    </v-app>
+  </div>
 </template>
 
 <style>
@@ -32,7 +52,11 @@
 </style>
 
 <script>
+import Header from '@/components/Header.vue'
 export default {
+  components:{
+    Header,
+  },
 	name: "App",
 	watch: {
 		$route: {
