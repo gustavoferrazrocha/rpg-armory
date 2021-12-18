3ac36b740c6d6feaba5f99fe9b54e723af3cf831
gustavoferrazrocha <gustavoguusfr@gmail.com>
2021-12-18 15:42:14 -0300

    chore: title page naem

--- src/router/index.ts
+++ src/router/index.ts
@@ -20,10 +20,19 @@ const routes: Array<RouteConfig> = [
   }
 ]
 
+
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
+const DEFAULT_TITLE = 'Some Default Title';
+router.afterEach((to, from) => {
+    // Use next tick to handle router history correctly
+    // see: https://github.com/vuejs/vue-router/issues/914#issuecomment-384477609
+    Vue.nextTick(() => {
+        document.title = to.meta?.title || DEFAULT_TITLE;
+    });
+});
 
 export default router
