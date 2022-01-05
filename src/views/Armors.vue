706c115bd545cf7907851dfdade2cafc1d95ab7e
gustavoferrazrocha <gustavoguusfr@gmail.com>
2022-01-05 13:24:50 -0300

    feat: implement api and table

--- src/views/Armors.vue
+++ src/views/Armors.vue
@@ -1,22 +1,60 @@
 <template>
-    <div class="main">
-        <h1>Armors</h1>
-    </div>
+  <div class="main">
+    <h1>Armors</h1>
+    <v-card>
+      <v-card-title>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table
+        :headers="headers"
+        :items="armors"
+        :search="search"
+      ></v-data-table>
+    </v-card>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-   methods:{
-    
+  data() {
+    return {
+      armors: [],
+      search: '',
+      headers: [
+          {
+            text: 'Nome',
+            align: 'start',
+            value: 'name',
+          },
+          { text: 'Preço', value: 'price' },
+          { text: 'Classe de Armadura (CA)', value: 'armorclass' },
+          { text: 'Força', value: 'stregnth' },
+          { text: 'Furtividade', value: 'furtivity' },
+          { text: 'Peso', value: 'weight' },
+        ],
+    };
   },
-   created(){
-    console.log('api')
-  }
-}
+  mounted() {
+    axios
+      .get("https://www.dnd5eapi.co/api/equipment-categories/armor")
+      .then((response) => (this.armors = response.data.equipment))
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
+};
 </script>
 
 <style >
-.main{
-    margin-top: 3rem,
+p {
+  margin-top: 3rem;
 }
 </style>
